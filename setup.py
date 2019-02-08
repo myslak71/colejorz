de29cbd27281eaafc5f1edefae23307338fb5e7d
Łukasz Bieszczad <l.bieszczad@clearcode.cc>
2019-02-08 15:23:35 +0100

    Use entry point when running the server

--- setup.py
+++ setup.py
@@ -13,9 +13,15 @@ setup(
     name='colejorz',
     version='0.0.0',
     description='To be determined.',
+    package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=requirements,
     extras_require={
         'tests': requirements_tests
+    },
+    entry_points={
+        'console_scripts': [
+            'colejorz = colejorz.stationmaster:main',
+        ]
     }
 )
