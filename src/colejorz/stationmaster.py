bee223283456d6b7698eafaef3cb02a20bd4fc5f
myslak71 <myslak@protonamil.com>
2019-09-21 08:34:39 +0200

    Update typing to python 3.6

--- src/colejorz/stationmaster.py
+++ src/colejorz/stationmaster.py
@@ -14,7 +14,7 @@ class StationMaster:
 
     def __init__(self):
         """Initialize stationmaster, queue and Pilothouse."""
-        self._queue = Queue()  # type: Queue[Dict[str, int]]
+        self._queue: Queue[Dict[str, int]] = Queue()  # pylint:disable=E1136
         self._pilothouse = Pilothouse(self._queue)
 
     @property
